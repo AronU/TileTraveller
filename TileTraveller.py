ff4a4572acbece06590ef3fefb0897f00a9474c2
AronU <aronu97@hotmail.com>
2018-09-18 14:12:57 +0000

    Issues have been fixed. Moved the directions to the correct locations to avoid an error message. Project is finished.

--- TileTraveller.py
+++ TileTraveller.py
@@ -6,88 +6,95 @@
 
 tile_int = 1
 direction_str = ""
-
+print("You can travel: (N)orth.")
 while tile_int < 9:
     if tile_int == 1:
-        print("You can travel: (N)orth.")
         direction_str = str(input("Direction: "))
         direction_str = direction_str.upper()
         if direction_str == "N":
             tile_int = 2
+            print("You can travel: (N)orth or (E)ast or (S)outh.")
         else:
             print("Not a valid direction!")
 
     elif tile_int == 2:
-        print("You can travel: (N)orth or (E)ast or (S)outh.")
         direction_str = str(input("Direction: "))
         direction_str = direction_str.upper()
         if direction_str == "N":
             tile_int = 3
+            print("You can travel: (E)ast or (S)outh.")
         elif direction_str == "E":
             tile_int = 5
+            print("You can travel: (S)outh or (W)est.")
         elif direction_str == "S":
             tile_int = 1
+            print("You can travel: (N)orth.")
         else:
             print("Not a valid direction!")
     elif tile_int == 3:
-        print("You can travel: (E)ast or (S)outh.")
         direction_str = str(input("Direction: "))
         direction_str = direction_str.upper()
         if direction_str == "E":
             tile_int = 4
+            print("You can travel: (E)ast or (W)est.")
         elif direction_str == "S":
             tile_int = 2
+            print("You can travel: (N)orth or (E)ast or (S)outh.")
         else:
             print("Not a valid direction!")
     elif tile_int == 4:
-        print("You can travel: (E)ast or (W)est.")
         direction_str = str(input("Direction: "))
         direction_str = direction_str.upper()
         if direction_str == "E":
             tile_int = 7
+            print("You can travel: (S)outh or (W)est.")
         elif direction_str == "W":
             tile_int = 3
+            print("You can travel: (E)ast or (S)outh.")
         else:
             print("Not a valid direction!")
     elif tile_int == 5:
-        print("You can travel: (S)outh or (W)est.")
         direction_str = str(input("Direction: "))
         direction_str = direction_str.upper()
         if direction_str == "S":
             tile_int = 6
+            print("You can travel: (N)orth.")
         elif direction_str == "W":
             tile_int = 2
+            print("You can travel: (N)orth or (E)ast or (S)outh.")
         else:
             print("Not a valid direction!")
     elif tile_int == 6:
-        print("You can travel: (N)orth.")
         direction_str = str(input("Direction: "))
         direction_str = direction_str.upper()
         if direction_str == "N":
             tile_int = 5
+            print("You can travel: (S)outh or (W)est.")
         else:
             print("Not a valid direction!")
     elif tile_int == 7:
-        print("You can travel: (W)est or (S)outh.")
         direction_str = str(input("Direction: "))
         direction_str = direction_str.upper()
         if direction_str == "S":
             tile_int = 8
+            print("You can travel: (N)orth or (S)outh.")
         elif direction_str == "W":
             tile_int = 4
+            print("You can travel: (E)ast or (W)est.")
         else:
             print("Not a valid direction!")
     elif tile_int == 8:
-        print("You can travel: (N)orth or (S)outh.")
         direction_str = str(input("Direction: "))
         direction_str = direction_str.upper()
         if direction_str == "N":
             tile_int = 7
+            print("You can travel: (S)outh or (W)est.")
         elif direction_str == "S":
             tile_int = 9
+            print("Victory!")
         else:
             print("Not a valid direction!")
-print("Victory!")
+
         
 
 
